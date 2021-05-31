d5dcb9cbd590aacd364d67935eaf36f86fcf544a
hhv19 <hhv19@ic.ac.uk>
2021-05-31 14:44:17 +0100

    Seperate unit/integration tests and configure gitlabCI testing

--- test/unit_tests/test_example2.py
+++ test/unit_tests/test_example2.py
@@ -3,10 +3,6 @@ import unittest
 from niched.example2 import dec
 
 
-class MyTestCase(unittest.TestCase):
+class Example2TestCase(unittest.TestCase):
     def test_dec(self):
         self.assertEqual(0, dec(1))
-
-
-if __name__ == '__main__':
-    unittest.main()

53f098895f0ddf84db0843ec937bfccb77befb8c
hhv19 <hhv19@ic.ac.uk>
2021-06-01 16:03:23 +0100

    Implement endpoint for signup and login

--- niched/api/routers/api.py
+++ niched/api/routers/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from niched.api.routers import users
+from niched.api.routers import auth
 
 router = APIRouter()
-router.include_router(users.router, prefix="/users")
+router.include_router(auth.router, prefix="/auth")

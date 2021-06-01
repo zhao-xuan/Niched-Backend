ea717921c316522dbe7ac063a501f251f2694869
hhv19 <hhv19@ic.ac.uk>
2021-06-01 18:21:49 +0100

    Dependency inversion on 'users_' methods for easier testing

--- niched/database/mongo.py
+++ niched/database/mongo.py
@@ -2,23 +2,19 @@ import logging
 
 from pymongo import MongoClient
 
-from niched.core.config import DB_CONNECTION_STRING
+from niched.core.config import DB_CONNECTION_STRING, DB_DATABASE
 
 logger = logging.getLogger(__name__)
 
-client = MongoClient()
+
+class MongoConnection:
+
+    client = MongoClient(DB_CONNECTION_STRING)
+    db = client[DB_DATABASE]
+
+    @staticmethod
+    def get_users_collection():
+        return MongoConnection.db["users"]
 
 
-def db_connect():
-    global client
-    logger.info("Initiating connection with database...")
-    try:
-        client = MongoClient(DB_CONNECTION_STRING)
-        logger.info("Established connection with database")
-    except Exception as e:
-        logger.error("Failed to establish connection with database: ", e)
-
-
-def db_terminate():
-    global client
-    client.close()
+conn = MongoConnection()
--- niched/main.py
+++ niched/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 
 from niched.api.routers.api import router as api_router
-from niched.database.mongo import db_connect, db_terminate
 
 
 def create_app():
     fast_app = FastAPI()
 
-    fast_app.add_event_handler("startup", db_connect)
-    fast_app.add_event_handler("shutdown", db_terminate)
+    # Add event handlers or middlewares here
 
     fast_app.include_router(api_router)
     return fast_app

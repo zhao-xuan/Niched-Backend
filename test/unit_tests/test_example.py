0d2b93f5a207b5ddebe63897c3ea9e154f41c0f3
hhv19 <hhv19@ic.ac.uk>
2021-06-02 09:49:06 +0100

    Add tests for user authentication

--- test/unit_tests/test_example.py
+++ test/unit_tests/test_example.py
@@ -3,6 +3,6 @@ import unittest
 from niched.example import inc
 
 
-class ExampleTestCase(unittest.TestCase):
+class TestCase(unittest.TestCase):
     def test_inc(self):
         self.assertEqual(1, inc(0))

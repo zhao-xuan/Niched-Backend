c5582d53fe15f982ecbb6ac432c8f6bc27a95a88
zhao-xuan <zhaoxuan0914@hotmail.com>
2021-06-02 09:49:06 +0100

    Add tests for user authentication

--- test/integration_tests/test_app.py
+++ test/integration_tests/test_app.py
@@ -7,7 +7,11 @@ from niched.main import app
 client = TestClient(app)
 
 
-class ExampleClientTestCase(unittest.TestCase):
-    def test_user_leo(self):
-        response = client.get("/users/leo")
+class UserAuthenticationTestCase(unittest.TestCase):
+
+    def test_login_successful(self):
+        response = client.post("/auth/login",
+                               headers={"Content-Type": "application/x-www-form-urlencoded",
+                                        "accept": "application/json"},
+                               data="username=test&password=test")
         assert response.status_code == 200

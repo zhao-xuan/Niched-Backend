fd8bb54a2d20e90f1ff0fabc8b04fb3da74f79e0
zhao-xuan <zhaoxuan0914@hotmail.com>
2021-06-02 09:48:14 +0100

    Configure global logging format

--- niched/core/__init__.py
+++ niched/core/__init__.py
@@ -4,8 +4,10 @@ import sys
 from niched.core.config import LOGGING_LEVEL
 
 logger = logging.getLogger(__name__)
+
+log_format = '[%(levelname)s] %(asctime)s - %(name)s : %(message)s'
+logging.basicConfig(format=log_format, level=LOGGING_LEVEL)
+
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(LOGGING_LEVEL)
-formatter = logging.Formatter('[%(levelname)s] - %(asctime)s - %(name)s : %(message)s')
-handler.setFormatter(formatter)
 logger.addHandler(handler)

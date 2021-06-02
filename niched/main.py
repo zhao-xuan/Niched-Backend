98f1113ac942c2b0b496078f85bf575df636a2d0
Wu, Gavin <gavin.wu19@imperial.ac.uk>
2021-06-02 20:04:35 +0000

    Removed Async functions

--- niched/main.py
+++ niched/main.py
@@ -14,12 +14,4 @@ def create_app():
 
 app = create_app()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
-
     

1a711e7041098b620d414186b6f019e695a1b430
gjw19 <gjw19@ic.ac.uk>
2021-06-02 18:28:45 +0100

    Functionality for creating and getting group data

--- niched/main.py
+++ niched/main.py
@@ -13,3 +13,13 @@ def create_app():
 
 
 app = create_app()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+    

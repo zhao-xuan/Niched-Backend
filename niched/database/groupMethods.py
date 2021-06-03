6ebdd20866b4c92aa95bd8e730dba7430b4688c5
gjw19 <gjw19@ic.ac.uk>
2021-06-03 16:36:01 +0100

    Group now also store date and time of creation, generated by backend

--- niched/database/groupMethods.py
+++ niched/database/groupMethods.py
@@ -1,15 +1,25 @@
 import logging
+from datetime import datetime
 from typing import Optional
 
 from pymongo.collection import Collection
 
-from niched.models.schema.groups import GroupData
+from niched.models.schema.groups import GroupDataDB, GroupFormData
 
 logger = logging.getLogger(__name__)
 
 
-def create_group(groups: Collection, group_details: GroupData) -> bool:
-    group_dict = group_details.dict()
+def create_group(groups: Collection, group_details: GroupFormData) -> bool:
+
+    group_data_insert = GroupDataDB(
+        group_id     = group_details.group_id,
+        name         = group_details.name,
+        description  = group_details.group_id,
+        image_url    = group_details.image_url,
+        creation_date=datetime.utcnow()  ) # group_details.dict()
+
+    group_dict = group_data_insert.dict()
+
     try:
         groups.insert_one(group_dict)
         logger.info(f"Group {group_details.name} created successfully!")
@@ -19,10 +29,10 @@ def create_group(groups: Collection, group_details: GroupData) -> bool:
         return False
 
 
-def get_group(groups: Collection, group_id: str) -> Optional[GroupData]:
+def get_group(groups: Collection, group_id: str) -> Optional[GroupDataDB]:
     try:
         group_json = groups.find_one({"group_id": group_id})
-        return GroupData(**group_json) if group_json else None
+        return GroupDataDB(**group_json) if group_json else None
     except Exception as e:
         logger.error(f"Exception raised when fetching group {group_id}: {e}")
         return None

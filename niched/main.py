6a6ac2a823d8eb4e5efc4df6b388aff8f822ffef
Tom Zhao <zhaoxuan0914@hotmail.com>
2021-06-03 16:45:34 +0100

    xz1919: addded CORS and changed database url in config.py

--- niched/main.py
+++ niched/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 
 from niched.api.routers.api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app():
     fast_app = FastAPI()
+
+    origins = ["*"]
+
+    fast_app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     # Add event handlers or middlewares here
 

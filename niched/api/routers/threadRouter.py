f8b2c8a1ac714f0cbcb4de6aab6c7aee88ec7d69
gjw19 <gjw19@ic.ac.uk>
2021-06-04 12:34:25 +0100

    Added return of thread id after creation

--- niched/api/routers/threadRouter.py
+++ niched/api/routers/threadRouter.py
@@ -1,10 +1,15 @@
 import logging
 from niched.database.threadMethods import create_thread
+from bson.objectid import ObjectId
 from typing import Dict
 
-from fastapi import APIRouter, HTTPException
+from bson import json_util
+import json 
 
-from niched.database.threadMethods import create_thread
+from fastapi import APIRouter, HTTPException, Body, FastAPI, status
+from fastapi.responses import JSONResponse
+
+from niched.database.threadMethods import create_thread, get_thread
 from niched.database.mongo import conn
 from niched.models.schema.threads import ThreadDataDB, ThreadFormData
 
@@ -17,8 +22,50 @@ Thread endpoint functions. The following functions should be created:
 1. get_threads_in_group()
 2. get_thread()
 3. create_thread()
-
 """
+
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+
+ 
+"""
+---- returns a HTTP 201 status code, with header containing ObjectID of new thread.
+Success: HTTP 201 with response body:
+{
+  "$oid": "60ba0e2233aa9e2863ab2185"
+}
+"""
+@router.post("/new")
+def create_new_thread(thread_details: ThreadFormData):
+    threads_collection = conn.get_threads_collection()
+
+    # check thread_details.group_id is a valid group first!
+
+    try:
+        _id = create_thread(threads_collection, thread_details)
+        # check if an object ID was returned, and not empty
+        if _id:
+            return JSONResponse(status_code=status.HTTP_201_CREATED, content=parse_json(_id))
+        else:
+            logger.error(f"Failed to assign object ID (insertion into database failed)")
+            raise HTTPException(status_code=400, detail=f"Failed to create new thread")
+    except Exception as e:
+        logger.error(f"Cannot create new thread {e}")
+        raise HTTPException(status_code=400, detail=f"Failed to create new thread {e}")
+
+
+@router.get("/{thread_id}/", response_model=ThreadDataDB)
+def get_group_with_id(thread_id: str):
+    threads_collection = conn.get_threads_collection()
+
+    thread_data = get_thread(threads_collection, thread_id)
+
+    if thread_data is None:
+        raise HTTPException(status_code=400, detail="Thread does not exist")
+
+    return thread_data
 
 
 # @router.get("/{group_id}", response_model=Dict[str, GroupDataDB])
@@ -31,22 +78,4 @@ Thread endpoint functions. The following functions should be created:
 #     return all_groups
 
 
-# @router.get("/{group_id}", response_model=GroupDataDB)
-# def get_group_with_id(group_id: str):
-#     groups_collection = conn.get_groups_collection()
-#     group_data = get_group(groups_collection, group_id)
-#     if group_data is None:
-#         raise HTTPException(status_code=400, detail="Group does not exist")
 
-#     return group_data
-
-
-@router.post("/new")
-def create_new_thread(thread_details: ThreadFormData):
-    threads_collection = conn.get_threads_collection()
-
-    try:
-        create_thread(threads_collection, thread_details)
-    except Exception as e:
-        logger.error(f"Cannot create new thread {e}")
-        raise HTTPException(status_code=400, detail=f"Failed to create new thread {e}")
--- niched/database/threadMethods.py
+++ niched/database/threadMethods.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 from typing import Optional, List
 
+
 from pymongo.collection import Collection
 
 from niched.models.schema.threads import ThreadFormData, ThreadDataDB
@@ -9,7 +10,8 @@ from niched.models.schema.threads import ThreadFormData, ThreadDataDB
 logger = logging.getLogger(__name__)
 
 
-def create_thread(threads: Collection, thread_details: ThreadFormData) -> bool:
+
+def create_thread(threads_collection: Collection, thread_details: ThreadFormData) -> str:
     thread_data_insert = ThreadDataDB(
         group_id=thread_details.group_id,
         title=thread_details.title,
@@ -19,21 +21,23 @@ def create_thread(threads: Collection, thread_details: ThreadFormData) -> bool:
     thread_dict = thread_data_insert.dict()
 
     try:
-        threads.insert_one(thread_dict)
+        # collection.insert returns the id of the new object inserted into the database
+        _id = threads_collection.insert(thread_dict)
         logger.info(f"Thread {thread_details.title} created successfully!")
-        return True
+        print(_id)
+        return _id
     except Exception as e:
         logger.error(f"Cannot create thread {thread_details.title}, exception raised {e}")
-        return False
+        return ""
 
 
-# def get_group(groups: Collection, group_id: str) -> Optional[GroupDataDB]:
-#     try:
-#         group_json = groups.find_one({"group_id": group_id})
-#         return GroupDataDB(**group_json) if group_json else None
-#     except Exception as e:
-#         logger.error(f"Exception raised when fetching group {group_id}: {e}")
-#         return None
+def get_thread(threads_collection: Collection, thread_id: str) -> Optional[ThreadDataDB]:
+    try:
+        thread_json = threads_collection.find_one({"_id": thread_id})
+        return ThreadDataDB(**thread_json) if thread_json else None
+    except Exception as e:
+        logger.error(f"Exception raised when fetching thread {thread_id}: {e}")
+        return None
 
 
 # def get_all_groups_in_db(groups: Collection) -> List[GroupDataDB]:

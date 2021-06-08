e3db04d7fc65855f681d37757f57a1182930ef9a
zhao-xuan <zhaoxuan0914@hotmail.com>
2021-06-08 10:32:43 +0100

    Initial event endpoint with /new implemented

--- niched/api/routers/api.py
+++ niched/api/routers/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from niched.api.routers import auth, group, thread
+from niched.api.routers import auth, group, thread, events
 
 router = APIRouter()
 router.include_router(auth.router, prefix="/auth")
 router.include_router(group.router, prefix="/group")
 router.include_router(thread.router, prefix="/thread")
+router.include_router(events.router, prefix="/events")

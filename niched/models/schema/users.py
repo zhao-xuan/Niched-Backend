205232b4604286aef124ac277bf302a730cdd934
Tom Zhao <zhaoxuan0914@hotmail.com>
2021-06-11 12:10:47 +0100

    xz1919: fixed an error in users.py UserToken definition

--- niched/models/schema/users.py
+++ niched/models/schema/users.py
@@ -3,12 +3,6 @@ from typing import Optional, List
 from pydantic import BaseModel, EmailStr
 
 
-class UserToken(BaseModel):
-    access_token: str
-    token_type: str
-    user_details: UserDetails
-
-
 class UserDetails(BaseModel):
     user_name: str
     email: Optional[EmailStr]
@@ -24,6 +18,10 @@ class UserDetails(BaseModel):
             }
         }
 
+class UserToken(BaseModel):
+    access_token: str
+    token_type: str
+    user_details: UserDetails
 
 class UserDetailsDB(UserDetails):
     password: str

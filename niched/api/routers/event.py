f084dfc5ce77becb18304a7c987bec27e4a983bd
hhv19 <hhv19@ic.ac.uk>
2021-06-11 17:20:52 +0100

    Add validations for new events

--- niched/api/routers/event.py
+++ niched/api/routers/event.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, HTTPException
 from starlette.status import (
-    HTTP_201_CREATED, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_404_NOT_FOUND, HTTP_200_OK
+    HTTP_201_CREATED, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_404_NOT_FOUND, HTTP_200_OK, HTTP_400_BAD_REQUEST
 )
 
-from niched.database.event_utils import create_event, check_event_id_exist, get_event_with_id
+from niched.database.event_utils import create_event, check_event_id_exist, get_event_with_id, InvalidEventException
 from niched.database.mongo import conn
+from niched.database.user_utils import check_user_id_exist
 from niched.models.schema.events import EventIn, EventOut
 
 router = APIRouter()
@@ -13,13 +14,18 @@ router = APIRouter()
 @router.post("/", response_model=EventOut, status_code=HTTP_201_CREATED, name="event:create")
 def new_event(event_data: EventIn):
     event_coll = conn.get_events_collection()
-    event = create_event(event_coll, event_data)
+    users_coll = conn.get_users_collection()
 
-    if not event:
-        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail={"msg": "Failed to create event, check logs for more detail!"})
+    if not check_user_id_exist(users_coll, event_data.author_id):
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND,
+                            detail={"msg": "Invalid user ID!"})
 
-    return event
+    try:
+        event = create_event(event_coll, event_data)
+        return event
+    except InvalidEventException as e:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,
+                            detail={"msg": e.message})
 
 
 @router.get("/{event_id}", response_model=EventOut, status_code=HTTP_200_OK, name="event:getById")

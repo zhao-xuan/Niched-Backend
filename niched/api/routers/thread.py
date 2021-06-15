d6f2160405af16f56479f9c87bef41e960a4738c
zhao-xuan <zhaoxuan0914@hotmail.com>
2021-06-15 10:32:23 +0100

    Add endpoints for adding comments

--- niched/api/routers/thread.py
+++ niched/api/routers/thread.py
@@ -1,13 +1,16 @@
 import logging
+from typing import List
 
 from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 from starlette.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
 
+from niched.database.comment_utils import get_comments_in_thread
 from niched.database.group_utils import check_group_id_exist
 from niched.database.mongo import conn
 from niched.database.thread_utils import create_thread, get_thread, check_thread_id_exist, remove_thread
 from niched.database.user_utils import check_user_id_exist
+from niched.models.schema.comments import CommentOut, CommentIn
 from niched.models.schema.threads import ThreadIn, ThreadOut
 
 router = APIRouter()
@@ -68,3 +71,16 @@ def remove_thread_with_id(thread_id: str):
     logger.error(f"Server failed to delete thread with ID {thread_id}")
     raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR,
                         detail={"msg": "Server failed to delete new thread"})
+
+
+@router.get("/{thread_id}/comments", response_model=List[CommentOut], status_code=HTTP_200_OK,
+            name="thread:getComments")
+def get_all_comments_in_thread(thread_id: str):
+    threads_collection = conn.get_threads_collection()
+    comments_collection = conn.get_comments_collection()
+
+    if not check_thread_id_exist(threads_collection, thread_id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"msg": "Thread ID does not exist"})
+
+    return get_comments_in_thread(comments_collection, thread_id)
+

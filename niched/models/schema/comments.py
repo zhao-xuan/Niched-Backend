336209526aed9b21a93d081ea2277f22903b2b7f
hhv19 <hhv19@ic.ac.uk>
2021-06-15 10:32:23 +0100

    Add endpoints for adding comments

---
+++ niched/models/schema/comments.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from bson import ObjectId
+from bson.errors import InvalidId
+from pydantic import BaseModel
+
+
+class OID(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        try:
+            return ObjectId(str(v))
+        except InvalidId:
+            raise ValueError("Not a valid ObjectId")
+
+
+class CommentIn(BaseModel):
+    thread_id: str
+    user_name: str
+    body: str
+
+
+class CommentDB(CommentIn):
+    thread_id: OID
+    user_name: str
+    body: str
+    creation_date: datetime
+
+
+class CommentOut(CommentIn):
+    comment_id: str
+    creation_date: datetime
